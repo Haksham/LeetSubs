676c066a988811e77dcaa001d53a7322b217364e
Harsh_V_M <harsh924hashvm@gmail.com>
2025-02-16 21:35:23 +0530

    Time: 6 ms (52.34%) | Memory: 17.7 MB (89.84%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
@@ -1,34 +1,27 @@
 class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
-        res = None
-        arr = [-1] * (2 * n - 1)
-        used = [False] * (n + 1)
-
-        def backtrack(l):
-            nonlocal res
-            if l == len(arr):
-                res = arr[:]
+        res = [0] * (2 * n - 1)
+        seen = set()
+        def backtrack(i):
+            if i == len(res):
                 return True
-
-            if arr[l] != -1:
-                return backtrack(l + 1)
-
-            for num in range(n, 0, -1):
-                if used[num]:
+            if res[i]:
+                return backtrack(i+1)
+            for j in range(n,0,-1):
+                if j in seen:
                     continue
-
-                r = l + num if num > 1 else l
-                if r < len(arr) and arr[r] == -1:
-                    arr[l], arr[r] = num, num
-                    used[num] = True
-
-                    if backtrack(l + 1):
+                seen.add(j)
+                res[i] = j
+                if j == 1:
+                    if backtrack(i+1):
                         return True
-
-                    arr[l], arr[r] = -1, -1
-                    used[num] = False
-
+                elif j + i < len(res) and res[i+j] == 0:
+                    res[i+j] = j
+                    if backtrack(i+1):
+                        return True
+                    res[i+j] = 0
+                res[i] = 0
+                seen.remove(j)
             return False
-
         backtrack(0)
         return res
