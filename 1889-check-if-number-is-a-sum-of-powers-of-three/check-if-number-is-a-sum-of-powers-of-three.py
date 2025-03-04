778cb15ec80bf7558722143ee84427b28666ffa8
Harsh_V_M <harsh924hashvm@gmail.com>
2025-03-04 08:37:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (59.96%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -5,15 +5,11 @@ class Solution:
         while(n>=3**i):
             ls.append(3**i)
             i+=1
-        print(ls)
-        t=n
         for i in range(len(ls)-1,-1,-1):
-            if t==0:
+            if n==0:
                 return True
-            if ls[i]<=n:
-                if t-ls[i]>=0:
-                    t=t-ls[i]
-                    if t==0:
-                        return True
-            print(t,ls[i])
+            if ls[i]<=n and n-ls[i]>=0:
+                n-=ls[i]
+                if n==0:
+                    return True
         return False
