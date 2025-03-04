1147f1dbb596cb094f08d9c031c88e3243bc92f0
Harsh_V_M <harsh924hashvm@gmail.com>
2025-03-04 06:18:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (63.02%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort()
+        for i,v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+        return 0
+
+        
