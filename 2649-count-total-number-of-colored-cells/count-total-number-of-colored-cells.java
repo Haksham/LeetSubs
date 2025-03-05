82035725c071cdfcd56df074fe57adbe1ed4e16f
Harsh_V_M <harsh924hashvm@gmail.com>
2025-03-05 06:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (26.23%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,12 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        long ans=0;
-        long i;
-        for(i=1;i<n;i++){
-            ans+=(2*i-1);
-        }
-        ans*=2;
-        ans+=(2*i-1);
-        return ans;
+        return 2*(long)n*(n-1)+1;
     }
 }
