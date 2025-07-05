6150a6a60434478cdad67e804bade2f32435dc6a
Harsh_V_M <harsh924hashvm@gmail.com>
2025-07-05 11:31:25 +0530

    Time: 3 ms (34.37%) | Memory: 18 MB (9.60%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -7,13 +7,9 @@ class Solution:
                 dic[i]=1
             else:
                 dic[i]+=1
-
         k=list(dic.keys())
         v=list(dic.values())
-        print(dic)
-        print(k,"\n",v)
         for i in range(len(k)-1,-1,-1):
             if k[i]==v[i] and ans<k[i]:
-                print(k[i],v[i])
                 ans=k[i]
         return ans
