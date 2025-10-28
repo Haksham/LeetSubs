fcff6e4e34c2736c8256b7b157247a578437a0ce
Harsh_V_M <harsh924hashvm@gmail.com>
2025-10-28 06:27:10 +0530

    Time: 45 ms (73.94%) | Memory: 17.9 MB (37.23%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        tot=sum(nums)
+        c=0
+        ans=0
+        for i in nums:
+            c+=i
+            if i==0 and tot==2*c:
+                ans+=2
+            elif i==0 and (tot==2*c+1 or tot==2*c-1):
+                ans+=1
+        return ans
